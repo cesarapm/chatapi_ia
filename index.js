b86971354210d3d1934ada4c7b51a5a5cd618072
cesarapm <garyfeomm@gmail.com>
2025-02-07 01:45:58 -0600

    Mensaje de commit

--- index.js
+++ index.js
@@ -2,8 +2,23 @@ require("dotenv").config();
 const express = require("express");
 const cors = require("cors");
 const bodyParser = require("body-parser");
+const helmet = require("helmet"); // Agregar helmet
 
 const app = express();
+
+// Configurar Content Security Policy (CSP)
+app.use(
+  helmet({
+    contentSecurityPolicy: {
+      directives: {
+        defaultSrc: ["'self'"],
+        styleSrc: ["'self'", "https://fonts.googleapis.com"],
+        fontSrc: ["'self'", "https://fonts.gstatic.com"],
+      },
+    },
+  })
+);
+
 app.use(cors());
 app.use(bodyParser.json());
 
@@ -12,9 +27,7 @@ const chatbotRoutes = require("./routes/chatbot");
 const otroChatRoutes = require("./routes/conectado");
 
 app.use("/chatbot", chatbotRoutes);
-
 app.use("/conectado", otroChatRoutes);
 
-
-
-app.listen(3001, () => console.log("Servidor en http://localhost:3001"));
+const PORT = process.env.PORT || 3001;
+app.listen(PORT, () => console.log(`Servidor en http://localhost:${PORT}`));
